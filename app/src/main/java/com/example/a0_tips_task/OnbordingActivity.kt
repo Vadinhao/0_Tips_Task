468b88dcfee527eeba5534c78417ffe2bc431563
VovaRuzhevich <vova.ruzhevich@gmail.com>
2021-07-13 01:00:55 +0300

    Adding new activity(OnbordingActivity)

---
+++ app/src/main/java/com/example/a0_tips_task/OnbordingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a0_tips_task
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OnbordingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onbording)
+    }
+}
