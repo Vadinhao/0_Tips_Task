4bca36c2ff964d4a1f53fa7aabf8cb7a30c80e67
VovaRuzhevich <vova.ruzhevich@gmail.com>
2021-07-13 05:48:19 +0300

    Adding onbordingActivity(kt and xml)

--- app/src/main/java/com/example/a0_tips_task/OnbordingActivity.kt
+++ app/src/main/java/com/example/a0_tips_task/OnbordingActivity.kt
@@ -2,10 +2,19 @@ package com.example.a0_tips_task
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a0_tips_task.databinding.ActivityOnbordingBinding
 
 class OnbordingActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityOnbordingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onbording)
+
+        binding = ActivityOnbordingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager2.adapter = ViewPagerAdapter()
     }
 }
