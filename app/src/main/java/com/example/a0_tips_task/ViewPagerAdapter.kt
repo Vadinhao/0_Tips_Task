c10a35fecc9b7b27adb5fe9956a08e79fb2f00cc
VovaRuzhevich <vova.ruzhevich@gmail.com>
2021-07-16 09:30:07 +0300

    changes for data class

--- app/src/main/java/com/example/a0_tips_task/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/a0_tips_task/ViewPagerAdapter.kt
@@ -6,26 +6,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a0_tips_task.data.dataSource
 
 class ViewPagerAdapter() : RecyclerView.Adapter<ViewPagerAdapter.ScreenViewHolder>() {
 
-    private val titles: ArrayList<Int> = arrayListOf(
-        R.string.title_for_onbording_1,
-        R.string.title_for_onbording_2,
-        R.string.title_for_onbording_3
-    )
-
-    private val images: ArrayList<Int> = arrayListOf(
-        R.drawable.onbording_step_1,
-        R.drawable.onbording_step_2,
-        R.drawable.onbording_step_3
-    )
-
-    private val texts: ArrayList<Int> = arrayListOf(
-        R.string.text_for_onbording_1,
-        R.string.text_for_onbording_2,
-        R.string.text_for_onbording_3
-    )
+    private var data = dataSource().loadData()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScreenViewHolder {
         val layout = LayoutInflater
@@ -35,19 +20,19 @@ class ViewPagerAdapter() : RecyclerView.Adapter<ViewPagerAdapter.ScreenViewHolde
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return data.size
     }
 
     override fun onBindViewHolder(holder: ScreenViewHolder, position: Int) {
         val context = holder.titleView.context
-        holder.titleView.text = context.getString(titles[position]);
-        holder.imgView.setImageResource(images[position])
-        holder.textView.text = context.getString(texts[position]);
+        holder.titleView.text = context.getString(data[position].titleResId)
+        holder.imgView.setImageResource(data[position].imgResId)
+        holder.textView.text = context.getString(data[position].textResId)
     }
 
     class ScreenViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        var titleView = view.findViewById<TextView>(R.id.textView_title)
-        var textView = view.findViewById<TextView>(R.id.textView_text)
-        var imgView = view.findViewById<ImageView>(R.id.imageView)
+        var titleView: TextView = view.findViewById<TextView>(R.id.textView_title)
+        var textView: TextView = view.findViewById<TextView>(R.id.textView_text)
+        var imgView: ImageView = view.findViewById<ImageView>(R.id.imageView)
     }
 }
