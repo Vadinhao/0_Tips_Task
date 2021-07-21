d2f43d6a8cafddb7605d7bc32208183c8189a577
VovaRuzhevich <vova.ruzhevich@gmail.com>
2021-07-21 03:58:17 +0300

    changes by Review-0 without dots

--- app/src/main/java/com/example/a0_tips_task/OnbordingActivity.kt
+++ app/src/main/java/com/example/a0_tips_task/OnbordingActivity.kt
@@ -13,29 +13,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.a0_tips_task.databinding.ActivityOnbordingBinding
 
-
 @SuppressLint("ResourceAsColor")
 class OnbordingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOnbordingBinding
+    //***
     private val APP_SETTINGS = "app_settings"
-
-    private fun getViewPagerItem(i: Int): Int {
-        return binding.viewPager2.currentItem + i
-    }
+    private val HAS_VISITED = "has_visited"
+    //***
+    private val NUM_OF_PAGES = 3
+    private val INDEX_OF_LAST_PAGE = NUM_OF_PAGES - 1
+    private val INDEX_OF_FIRST_PAGE = 0
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //***
         val startApp = Intent(this, MainActivity::class.java)
+        //***
         val sharPref = getSharedPreferences(
             APP_SETTINGS,
             Context.MODE_PRIVATE
         )
-        val hasVisited = sharPref.getBoolean("hasVisited", false)
-
-        if(hasVisited) {
+        val hasVisited = sharPref.getBoolean(HAS_VISITED, false)
+        if (hasVisited) {
             startActivity(startApp)
             finish()
         }
@@ -43,55 +44,53 @@ class OnbordingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_onbording)
         binding = ActivityOnbordingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //***
         binding.viewPager2.adapter = ViewPagerAdapter()
-
+        //***
         binding.buttonBack.setOnClickListener {
-            binding.viewPager2.setCurrentItem(getViewPagerItem(-1), true)
+            binding.viewPager2.setCurrentItem(binding.viewPager2.currentItem - 1, true)
         }
-
+        //***
         binding.buttonSkip.setOnClickListener {
             val e: SharedPreferences.Editor = sharPref.edit()
-            e.putBoolean("hasVisited", true)
+            e.putBoolean(HAS_VISITED, true)
             e.apply()
-
+            //***
             startActivity(startApp)
             finish()
         }
-
-        //костыль(2-ка)
+        //***
         binding.buttonNext.setOnClickListener {
-
-            if (getViewPagerItem(0) < 2) {
-                binding.viewPager2.setCurrentItem(getViewPagerItem(1), true)
+            if (binding.viewPager2.currentItem < INDEX_OF_LAST_PAGE) {
+                binding.viewPager2.setCurrentItem(binding.viewPager2.currentItem + 1, true)
             } else {
                 val e: SharedPreferences.Editor = sharPref.edit()
-                e.putBoolean("hasVisited", true)
+                e.putBoolean(HAS_VISITED, true)
                 e.apply()
-
+                //***
                 startActivity(startApp)
                 finish()
             }
         }
-        //костыль(промеж)
+        //***
         binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             @RequiresApi(Build.VERSION_CODES.M)
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-
-                if (position in 0..1) {
+                //Visibility of Skip button
+                if (position in INDEX_OF_FIRST_PAGE until INDEX_OF_LAST_PAGE) {
                     binding.buttonSkip.visibility = View.VISIBLE
                 } else {
                     binding.buttonSkip.visibility = View.INVISIBLE
                 }
-
-                if (position in 1..2) {
+                //Visibility of Back button
+                if (position in (INDEX_OF_FIRST_PAGE + 1)..INDEX_OF_LAST_PAGE) {
                     binding.buttonBack.visibility = View.VISIBLE
                 } else {
                     binding.buttonBack.visibility = View.INVISIBLE
                 }
 
-                setUpDots(position)
+                //setUpDots(position)
             }
         })
     }
@@ -101,14 +100,14 @@ class OnbordingActivity : AppCompatActivity() {
     fun setUpDots(position: Int) {
 
         val dots: Array<TextView> = arrayOf(
-            this.layoutInflater.inflate(R.xml.text_view, null) as TextView,
-            this.layoutInflater.inflate(R.xml.text_view, null) as TextView,
-            this.layoutInflater.inflate(R.xml.text_view, null) as TextView
+            this.layoutInflater.inflate(R.layout.text_view, null) as TextView,
+            this.layoutInflater.inflate(R.layout.text_view, null) as TextView,
+            this.layoutInflater.inflate(R.layout.text_view, null) as TextView
         )
 
         binding.linerLayout.removeAllViews()
 
-        for (i in 0..2) {
+        for (i in INDEX_OF_FIRST_PAGE..INDEX_OF_LAST_PAGE) {
             dots[i].text = getString(R.string.dot_for_onbording)
             dots[i].textSize = 35F
             dots[i].setTextColor(getColor(R.color.onborging_gray))
